9ce659a9fb6343f38d0cfd92c6a613cf83abddc8
LayoutXML <hello@rokasjankunas.com>
2020-07-14 21:21:35 +0300

    Init version

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM openjdk:16-slim
+
+ENV FOLDER_NAME="papermc_server"
+ENV PROJECT_NAME="paper" # paper/waterfall/travertine
+ENV MINECRAFT_VERSION=""
+ENV PAPERMC_VERSION="latest"
+ENV RAM="1024MB"
+ENV JAVA_ARGS=""
+
+RUN apt-get update
+RUN apt-get install -y wget
+RUN apt-get install -y jq
+
+COPY setup.sh ./
+RUN chmod +x ./setup.sh
+RUN ./setup.sh
+
+VOLUME ${FOLDER_NAME}
+EXPOSE 25565/tcp
+EXPOSE 25565/udp
---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+API_URL = "https://papermc.io/api/v1"
+
+if [ -n ${MINECRAFT_VERSION} ]; then
+  echo "Minecraft version is required."
+  exit 1
+fi
+
+mkdir -p ${FOLDER_NAME}
+cd ${FOLDER_NAME}
+
+if ! [ -f "${PROJECT_NAME}-${PAPERMC_VERSION}.jar" ]; then
+  wget "${API_URL}/${PROJECT_NAME}/${MINECRAFT_VERSION}/${PAPERMC_VERSION}/download" -O "${PROJECT_NAME}-${PAPERMC_VERSION}.jar"
+fi
+
+if ! [ -f "eula.txt" ]; then
+  echo "true" > "eula.txt"
+fi
+
+java -Xmx${RAM} -Xms${RAM} ${JAVA_OPTS} -jar "${PROJECT_NAME}-${PAPERMC_VERSION}.jar" --nogui
