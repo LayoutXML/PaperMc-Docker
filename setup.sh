c17fb15dcb82ced6b1174cceddacda1b64b6994a
LayoutXML <hello@rokasjankunas.com>
2020-07-14 22:44:25 +0300

    Variable fix

--- setup.sh
+++ setup.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-API_URL = "https://papermc.io/api/v1"
+API_URL="https://papermc.io/api/v1"
 
-if [ -n ${MINECRAFT_VERSION} ]; then
+if [ -n "$MINECRAFT_VERSION" ]; then
   echo "Minecraft version is required."
   exit 1
 fi
 
-mkdir -p ${FOLDER_NAME}
-cd ${FOLDER_NAME}
+mkdir -p "$FOLDER_NAME"
+cd "$FOLDER_NAME"
 
-if ! [ -f "${PROJECT_NAME}-${PAPERMC_VERSION}.jar" ]; then
-  wget "${API_URL}/${PROJECT_NAME}/${MINECRAFT_VERSION}/${PAPERMC_VERSION}/download" -O "${PROJECT_NAME}-${PAPERMC_VERSION}.jar"
+if ! [ -f "$PROJECT_NAME-$PAPERMC_VERSION.jar" ]; then
+  wget "$API_URL/$PROJECT_NAME/$MINECRAFT_VERSION/$PAPERMC_VERSION/download" -O "$PROJECT_NAME-$PAPERMC_VERSION.jar"
 fi
 
 if ! [ -f "eula.txt" ]; then
   echo "true" > "eula.txt"
 fi
 
-java -Xmx${RAM} -Xms${RAM} ${JAVA_OPTS} -jar "${PROJECT_NAME}-${PAPERMC_VERSION}.jar" --nogui
+java "-Xmx$RAM" "-Xms$RAM" "$JAVA_OPTS" -jar "$PROJECT_NAME-$PAPERMC_VERSION.jar" --nogui
